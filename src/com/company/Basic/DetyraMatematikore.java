20fb68137012d8f379e36894c5a1123044267c4e
AgimKryeziu <65826643+AgimKryeziu@users.noreply.github.com>
2021-10-18 16:47:03 +0200

    Klasa DetyraMatematikore

--- src/com/company/Basic/DetyraMatematikore.java
+++ src/com/company/Basic/DetyraMatematikore.java
@@ -22,13 +22,13 @@ public class DetyraMatematikore {
         //Te shkruhet programi i cili kalkulon formulën ne vijim( ku n – merret nga
         //shfrytëzuesi dhe n duhet te pranoj vlera nga 1 deri 100
 
-        int prodhimi = 1;
-        for (int x = 1; x <= numri; x++){
-            if (x == 1 || x != 2){
-                prodhimi *= (x - 2);
-            }
-        }
-        System.out.println(prodhimi);
+        //int prodhimi = 1;
+        //for (int x = 1; x <= numri; x++){
+        //    if (x == 1 || x != 2){
+          //      prodhimi *= (x - 2);
+         //   }
+       // }
+      //  System.out.println(prodhimi);
 
         //Te shkruhet progrmai I cili kalkulon formalin ne vijim ku n duhet te jipet dhe te
         //kete rang te vlerave nga 1 deri ne 500
@@ -38,7 +38,15 @@ public class DetyraMatematikore {
        //     zgjidhja = (Math.pow(x,2) - 2*x - 3) / 2*x -1;
       //  }
        // System.out.println("Zgjidhja: " + zgjidhja);
-
-
+        
+        System.out.print("Sheno nje vlere nga -100 deri ne 100: ");
+        int m = reader.nextInt();
+        
+        double shuma = 0;
+        numri = m * 5;
+        for (int x = m; x <= numri; x++) {
+            shuma = (Math.pow(x,2) - 2*x + 3)/ 2*x ;
+        }
+        System.out.println(shuma);
     }
 }
