539f248c4645814f098146fd02f5048f8002fb7b
AgimKryeziu <65826643+AgimKryeziu@users.noreply.github.com>
2021-10-20 15:13:43 +0200

    Klasa Chat

---
+++ src/com/company/Basic/Chat.java
@@ -0,0 +1,21 @@
+package com.company.Basic;
+
+import java.util.Scanner;
+
+public class Chat {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String stop = "Stop";
+        System.out.print("Personi1: ");
+        String personi1 = reader.nextLine();
+        System.out.print("Personi2: ");
+        String personi2 = reader.nextLine();
+
+        while (!personi2.equals(stop)){
+            System.out.print("Personi1: ");
+            personi1 = reader.nextLine();
+            System.out.print("Personi2: ");
+            personi2 = reader.nextLine();
+        }
+    }
+}
