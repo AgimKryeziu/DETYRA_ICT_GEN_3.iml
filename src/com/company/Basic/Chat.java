d5e434124da93289eddbd3cb6bfafd109d9fc02f
AgimKryeziu <65826643+AgimKryeziu@users.noreply.github.com>
2021-10-20 15:21:33 +0200

    Klasa Chat

--- src/com/company/Basic/Chat.java
+++ src/com/company/Basic/Chat.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Chat {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        String stop = "Stop";
         System.out.print("Personi1: ");
         String personi1 = reader.nextLine();
         System.out.print("Personi2: ");
         String personi2 = reader.nextLine();
 
-        while (!personi2.equals(stop)){
+        while (!personi2.equals("stop")){
             System.out.print("Personi1: ");
             personi1 = reader.nextLine();
             System.out.print("Personi2: ");
